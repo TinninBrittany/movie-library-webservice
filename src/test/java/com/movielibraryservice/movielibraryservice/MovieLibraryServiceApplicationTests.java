ab837bfae6d3876e57be67798ff71445a8ab574d
Brittany Tinnin <bntinnin7@gmail.com>
2022-05-16 17:54:26 -0700

    first commit

---
+++ src/test/java/com/movielibraryservice/movielibraryservice/MovieLibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movielibraryservice.movielibraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
