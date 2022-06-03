9c0c59a78687fa1597cdf66526640dbb1a47771b
Brittany Tinnin <bntinnin7@gmail.com>
2022-06-02 22:24:20 -0700

    add dependencies, datasource, and tutorial model

---
+++ src/main/java/com/movielibraryservice/movielibraryservice/model/Tutorial.java
@@ -0,0 +1,25 @@
+package com.movielibraryservice.movielibraryservice.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tutorial_generator")
+    private long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "published")
+    private boolean published;
+    public Tutorial() {
+    }
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+    // getters and setters
+}
